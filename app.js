27a52ccd1820cb42637e88ca59f6ea494bedb85d
angie-garciapaz <agarpaz06@gmail.com>
2021-10-17 21:33:35 -0600

    week 3

--- app.js
+++ app.js
@@ -3,24 +3,23 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const app = express();
+const errorController = require('./controllers/error');
 
-const adminData = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-
-app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.ejs'));
-});
+app.use(errorController.get404);
 
 // always on the bottom
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -2,21 +2,11 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
+const productsController = require('../controllers/products');
 
 const router = express.Router();
-const adminData = require('./admin');
 
-router.get('/', (req, res, next) => {
-    const products = adminData.products;
-    res.render('shop', {
-         prods: products,
-          pageTitle: 'Shop' ,
-          path: '/',
-          hasProducts: products.length > 0,
-          activeShop: true,
-          productCSS: true
-        });
-});
+router.get('/', productsController.getProdcuts);
 
 module.exports = router;
+
