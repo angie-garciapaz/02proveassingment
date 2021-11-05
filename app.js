2a4026acfba2003977bf17539535e6f5b318ecbe
angie-garciapaz <agarpaz06@gmail.com>
2021-11-04 22:19:45 -0600

    adding mongoose

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
 
 const app = express();
@@ -14,25 +14,52 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+// const authRoutes = require('./routes/auth');
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-    User.findById("617608504cbe6070404aaec5")
-        .then(user => {
-            req.user = new User(user.name, user.email, user.cart, user._id);
-            next();
-        })
-        .catch(err => console.log(err));
+  User.findById("61835dc2f1d3dd8bf30a76db")
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-
+// app.use(authRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-    app.listen(process.env.PORT || 5000);
-});
+// mongoConnect(() => {
+//     app.listen(process.env.PORT || 5000);
+// });
+
+mongoose
+  .connect(
+    'mongodb+srv://agarciapaz:DCXOUP3rXYX9f6Pl@cluster0.iiook.mongodb.net/shop?authSource=admin&replicaSet=atlas-bomvy4-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true'
+  )
+  .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Angie',
+          email: 'ag@test.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    })
+
+    app.listen(3000);
+    console.log('connected');
+  })
+  .catch(err => {
+    console.log(err);
+  });
--- controllers/shop.js
+++ controllers/shop.js
@@ -1,8 +1,11 @@
 const Product = require('../models/product');
+const Order = require('../models/order');
+
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
     .then(products => {
+      console.log(products);
       res.render('shop/product-list', {
         prods: products,
         pageTitle: 'All Products',
@@ -26,7 +29,7 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
     .then(products => {
       res.render('shop/index', {
         prods: products,
@@ -36,14 +39,15 @@ exports.getIndex = (req, res, next) => {
     })
     .catch(err => {
       console.log(err);
-    })
+    });
 };
 
 
 exports.getCart = (req, res, next) => {
   req.user
-    .getCart()
-    .then(products => {
+    .populate('cart.items.productId')
+    .then(user => {
+      const products = user.cart.items;
       res.render('shop/cart', {
         path: '/cart',
         pageTitle: 'Your Cart',
@@ -67,7 +71,8 @@ exports.postCart = (req, res, next) => {
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  req.user.deleteItemFromCart(prodId)
+  req.user
+    .removeFromCart(prodId)
     .then(result => {
       res.redirect('/cart');
     })
@@ -75,18 +80,32 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
-  let fetchedCart;
   req.user
-    .addOrder()
+    .populate('cart.items.productId')
+    .then(user => {
+      const products = user.cart.items.map(i => {
+        return { quantity: i.quantity, product: { ...i.productId._doc } };
+      });
+      const order = new Order({
+        user: {
+          name: req.user.name,
+          userId: req.user
+        },
+        products: products
+      });
+      order.save();
+    })
     .then(result => {
+      return req.user.clearCart();
+    })
+    .then(() => {
       res.redirect('/orders');
     })
     .catch(err => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
-  req.user
-    .getOrders()
+  Order.find({ "user.userId": req.user._id })
     .then(orders => {
       res.render('shop/orders', {
         path: '/orders',
